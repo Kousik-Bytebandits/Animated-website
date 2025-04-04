d557fb910497cf0bd03b15766d132c1899eba791
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-04-04 10:27:06 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,55 @@
+
+export default function AnimatedWebsite() {
+  return (
+    <div className="container">
+     
+      
+      {/* Hero Section */}
+      <section className="head">
+        <h1>CREATIVE <br/>DESIGN <br/> AGENCY.</h1>
+       
+      </section>
+      {/* image section*/}
+      <section className="image">
+        <img src="home-page.jpg"></img>
+      </section>
+      {/* About Section */}
+      <section className="about-section">
+      <div className="left-content">
+        <h2>DISCOVER <br/> OUR <span className="highlight">AGENCY</span></h2>
+        <p className="intro-text">
+          Welcome to <span className="highlight">PIXY</span> where your ideas come to life. We specialize in crafting unique brands, captivating advertising campaigns, and effective digital strategies.
+        </p>
+      </div>
+      <div className="right-content">
+        <p>
+          We specialize in crafting unique brands, captivating advertising campaigns, and effective digital strategies. Our team of talented designers, copywriters, and marketers works with passion and dedication to make your business stand out.
+        </p>
+        <p>
+          At <span className="highlight">PIXY</span>, we believe in the power of creativity and innovation. Our goal is to help you build an emotional connection with your audience using cutting-edge tools and approaches.We specialize in crafting unique brands, captivating advertising campaigns, and effective digital strategies. Our team of talented designers, copywriters, and marketers works with passion and dedication to make your business stand out. We take the time to understand your vision .
+
+
+        </p>
+      </div>
+    </section>
+      
+      {/* Services Section */}
+      <section className="section services">
+        <h2>Our Services</h2>
+       
+      </section>
+      
+      {/* Contact Section */}
+      <section className="section contact">
+        <h2>Contact Us</h2>
+        
+        
+      </section>
+      
+      {/* Footer */}
+      <footer className="footer">
+        &copy; 2025 Animated Website. All rights reserved.
+      </footer>
+    </div>
+  );
+}
